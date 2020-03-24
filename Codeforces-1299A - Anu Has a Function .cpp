6e873eb933fe009fd61d89d866ac662967ddee0c
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-03-24 05:05:47 -0700

    Update Codeforces-1299A - Anu Has a Function .cpp

--- Codeforces-1299A - Anu Has a Function .cpp
+++ Codeforces-1299A - Anu Has a Function .cpp
@@ -1,3 +1,21 @@
+/*
+Here a function f(x,y) has been stated as (x|y)-y ,right ? Now its array length is 10^5 .So it should be solved with O(n) or O(nlogn) time complexity !! So, if we check that (x|y)-y ,here (x|y) part should be independent .Let's see 
+
+x|y means where a bit in x or y or both is on that bit will be on in x|y ,right ?
+Now , when we subtract y from this , so - the bit which is now on in x|y and y will be zero after subtracting y from x|y.
+So , it makes sense to us that x|y has nothing to do with it ,because we subtarct again y from (x|y) . Therefore , 
+ (x|y) - y means ,from x those bit will be off which are on in y . ~y means flipping its all bits (the off bit will be on and on bit will be off ) . So if we now make x&(~y) is equal to (x|y)-y ,right ? f(x,y)=x&(~y)
+
+Now , think about array [a,b,c,d,e] .
+
+If we think this array order as right.
+
+Then ,value will be equal to  f(f(f(f(a,b),c),d),e) which is equivalent to a&(~b)&(~c)&(~d)&(~e) . From this expression , what will make you sense that we have to find out that value which will be first in array ,others will not make fact !! So, for traversing the whole array , we think this arri our first element and (make and operations with all ~(elements) ) .That's why make a suffix and prefix array of & operations of (~ all elements of till i) .Rest of this should be done by you 
+
+
+*/
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
